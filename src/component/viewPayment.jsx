1380b0d0dbbd1ba8f900d0cca7defb65a3e5499c
kavishka721 <67276427+kavishka721@users.noreply.github.com>
2021-05-23 18:35:51 +0530

    Update viewPayment.jsx

--- src/component/viewPayment.jsx
+++ src/component/viewPayment.jsx
@@ -3,23 +3,12 @@ import { render } from 'react-dom';
 import Createapayments from './createPayment';
 
 const ViewPayment = function(props) {
-        //console.log(props.paymentID);
-
-        
-            // const state  = {
-            // id : props.paymentID,
-            // name : props.cardHolderName
-            // }
-            const {data} = paymentData;
+       
             return (
-                
                 <div>
-                    <p>Payment Details</p>
-
-                    <p>{data.paymentID}</p>
-                    <p>{data.cardHolderName}</p>
+                    <p>Payment Success</p>
                 </div>
-            )
+            );
         
         
 
